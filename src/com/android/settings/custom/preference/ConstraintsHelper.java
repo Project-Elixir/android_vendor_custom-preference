PixelExperience/vendor_custom-preference

Revert "[REVERTME] Comment lineage hardware calls for now"
This reverts commit 47c5cf239f534d07203d859cdcf3f541620f46dd.

Change-Id: I01788e90f91822990de7e1e5419dfa17bfdda92f

src/com/android/settings/custom/preference/ConstraintsHelper.java
@@ -42,7 +42,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-//import com.android.internal.custom.hardware.LineageHardwareManager;
+import com.android.internal.custom.hardware.LineageHardwareManager;
 import com.android.settings.custom.R;
 
 
@@ -185,8 +185,8 @@ public class ConstraintsHelper {
                     rFeature = rFeature.substring(1);
                 }
                 boolean available = rFeature.startsWith("lineagehardware:") ?
-                        false/*LineageHardwareManager.getInstance(mContext).isSupported(
-                                rFeature.substring("lineagehardware:".length()))*/ :
+                        LineageHardwareManager.getInstance(mContext).isSupported(
+                                rFeature.substring("lineagehardware:".length())) :
                         hasSystemFeature(mContext, rFeature);
                 if (available == negated) {
                     return false;

Project-Elixir/android_vendor_custom-preference

src/com/android/settings/custom/preference/ConstraintsHelper.java
@@ -42,7 +42,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-//import com.android.internal.custom.hardware.LineageHardwareManager;
+import com.android.internal.lineage.hardware.LineageHardwareManager;
 import com.android.settings.custom.R;
 
 
@@ -185,8 +185,8 @@ public class ConstraintsHelper {
                     rFeature = rFeature.substring(1);
                 }
                 boolean available = rFeature.startsWith("lineagehardware:") ?
-                        false/*LineageHardwareManager.getInstance(mContext).isSupported(
-                                rFeature.substring("lineagehardware:".length()))*/ :
+                        LineageHardwareManager.getInstance(mContext).isSupported(
+                                rFeature.substring("lineagehardware:".length())) :
                         hasSystemFeature(mContext, rFeature);
                 if (available == negated) {
                     return false;